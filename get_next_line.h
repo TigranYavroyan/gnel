2a0c8c3a9ed241bf7b1e511da19d1eb3a618068d
Tigran Yavroyan <tyavroya@c2r5s2.42yerevan.am>
2024-02-19 18:06:12 +0400

    tmp

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tyavroya <tyavroya@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/09 17:25:06 by tyavroya          #+#    #+#             */
+/*   Updated: 2024/02/19 16:46:33 by tyavroya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif // BUFFER_SIZE
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+
+#endif // GET_NEXT_LINE_H
