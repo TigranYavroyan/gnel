62fb2749c72a85972e33b759f11b6a511ef0d9ee
Tigran Yavroyan <tyavroya@c2r5s6.42yerevan.am>
2024-02-27 16:42:31 +0400

    done get_next_line without bonus

--- get_next_line.c
+++ get_next_line.c
@@ -6,76 +6,44 @@
 /*   By: tyavroya <tyavroya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 17:25:40 by tyavroya          #+#    #+#             */
-/*   Updated: 2024/02/19 18:03:02 by tyavroya         ###   ########.fr       */
+/*   Updated: 2024/02/27 16:39:55 by tyavroya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen (char *str)
-{
-	size_t	count;
-
-	count = 0;
-	while (str[count] != '\n' && str[count] != '\0')
-	{
-		++count;
-	}
-	return (count);
-}
-
-void	ft_realloc(char **str, int from, int to)
-{
-	char	*new_string;
-	int		offset;
-	size_t	i;
-
-	i = 0;
-	offset = to - from;
-	new_string = (char *) malloc (offset + 1);
-	while (i < offset && (*str)[from] != '\0')
-	{
-		new_string[i] = (*str)[from];
-		++from;
-		++i;
-	}
-	new_string[offset] = '\0';
-	free(*str);
-	*str = new_string;
-}
-
 char	*get_next_line(int fd)
 {
-	static char	*str;
-	char		*new_line;
-	int			i;
+	static char	*line;
+	char		*next_lines;
 
-	if (BUFFER_SIZE < 1 || fd < 0 || read(fd, 0, 0) < 0)
-		return (NULL);
-	i = 0;
-	str = (char *) malloc(BUFFER_SIZE + 1);
-	read(fd, str, BUFFER_SIZE);
-	str[BUFFER_SIZE] = '\0';
-	printf("static memory -> %s", str);
-	str[BUFFER_SIZE] = '\0';
-	new_line = (char *) malloc(ft_strlen(str) + 1);
-	while (str[i] != '\n' && str[i] != '\0')
+	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, 0, 0) < 0)
+		return (0);
+	if (!line)
 	{
-		new_line[i] = str[i];
-		++i;
+		line = (char *)malloc(1);
+		if (!line)
+			return (0);
+		line[0] = '\0';
 	}
-	new_line[i] = '\0';
-	ft_realloc(&str, i + 1, BUFFER_SIZE);
-	return (new_line);
+	else
+	{
+		next_lines = ft_strchr(line, '\n');
+		if (!next_lines)
+		{
+			free(line);
+			return (line = 0);
+		}
+		else
+			new_line(&line, next_lines + 1);
+	}
+	ft_read(fd, &line);
+	return (ft_slice(&line));
 }
-
-int main () {
-	int fd = open("text.txt", O_RDONLY);
-	const char* str = get_next_line(fd);
-	printf("\n%s\n", str);
-	str = get_next_line(fd);
-	printf("\n%s\n", str);
-
-	close(fd);
-}
-
+// int main () {
+// 	int fd = open("text.txt", O_RDONLY);
+// 	for (int i = 0; i < 5; ++i) {
+// 		char *str = get_next_line(fd);
+// 		printf("%s", str);
+// 	}
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,16 @@
 /*   By: tyavroya <tyavroya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 17:25:06 by tyavroya          #+#    #+#             */
-/*   Updated: 2024/02/19 16:46:33 by tyavroya         ###   ########.fr       */
+/*   Updated: 2024/02/27 16:38:52 by tyavroya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif // BUFFER_SIZE
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif // BUFFER_SIZE
 
 # include <stdlib.h>
 # include <unistd.h>
@@ -23,5 +23,10 @@
 # include <stdio.h>
 
 char	*get_next_line(int fd);
+char	*ft_strchr(char *s, int c);
+char	*ft_strdup(char **line);
+void	ft_strjoin(char **line, char *buff);
+void	new_line(char **line, char *next_lines);
+void	ft_read(int fd, char **line);
 
 #endif // GET_NEXT_LINE_H
